e10588b9a21d7954c5a53cdbf04663c7564ac16d
ScottGrunewald <78197064+ScottGrunewald@users.noreply.github.com>
2021-01-29 02:00:34 -0500

    Update SampleClient.java
    Added code for Basic Tasks

--- src/main/java/SampleClient.java
+++ src/main/java/SampleClient.java
@@ -1,12 +1,31 @@
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
+import org.hl7.fhir.r4.formats.JsonParser;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Patient;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class SampleClient {
 
     public static void main(String[] theArgs) {
+
+/*        // Create a context
+        FhirContext ctx = FhirContext.forR4();
+
+        // Create a client
+        IGenericClient client = ctx.newRestfulGenericClient("https://hapi.fhir.org/baseR4");
+
+        // Read a patient with the given ID
+        Patient patient = client.read().resource(Patient.class).withId("example").execute();
+
+        // Print the output
+        String string = ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(patient);
+        System.out.println(string);*/
 
         // Create a FHIR client
         FhirContext fhirContext = FhirContext.forR4();
@@ -21,7 +40,40 @@ public class SampleClient {
                 .returnBundle(Bundle.class)
                 .execute();
 
+        // We now have the returned XML in the response variable, so we need to print it out.
 
+        List<Bundle.BundleEntryComponent> ourList = new ArrayList(response.getEntry());
+
+        ArrayList<String> ourPatients = new ArrayList();
+
+        for(int i=0;i<ourList.size();i++)
+        {
+            String string = fhirContext.newJsonParser().setPrettyPrint(true).encodeResourceToString(ourList.get(i).getResource());
+            StringBuffer patientInfo = new StringBuffer("");
+
+            if(string.contains("given"))
+            {
+                patientInfo.append(string.substring(string.indexOf("given")+19, string.indexOf("\"",string.indexOf("given")+19)));
+            }
+
+            patientInfo.append(" " + string.substring(string.indexOf("family")+10, string.indexOf("\"",string.indexOf("family")+10)));
+
+            if(string.contains("birthDate"))
+            {
+                patientInfo.append(" " + string.substring(string.indexOf("birthDate") + 13, string.indexOf("\"", string.indexOf("birthDate") + 13)));
+            }
+
+            ourPatients.add(patientInfo.toString());
+        }
+
+        Collections.sort(ourPatients);
+
+        for(int i=0;i<ourPatients.size();i++)
+        {
+            System.out.println(ourPatients.get(i));
+        }
+
+        System.out.println("Process Completed!");
     }
 
 }
